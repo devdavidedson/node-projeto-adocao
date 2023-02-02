fa0a120f6b3a6879ffc6ca5424955e0dd6df1a83
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-02-02 18:59:26 -0300

    ativando a lista no menu

--- src/controllers/searchControllers.ts
+++ src/controllers/searchControllers.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 
+import { Pet } from "../models/pet";
+
 export const search = (req: Request, res: Response) => {
     // res.render('pages/search)
 };
